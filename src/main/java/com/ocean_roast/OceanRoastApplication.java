78296a605dc36f7799098edae19eaae60806f871
Enis Arik <enisarik1987@gmail.com>
2025-01-09 23:48:14 +0300

    Revert "Created service."
    This reverts commit 470815b7a65a9459654786512752df10ca5dbae3.

--- src/main/java/com/ocean_roast/OceanRoastApplication.java
+++ src/main/java/com/ocean_roast/OceanRoastApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OceanRoastApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(OceanRoastApplication.class, args);
 	}
+
 }
--- src/main/java/com/ocean_roast/RoasteryController.java
+++ src/main/java/com/ocean_roast/RoasteryController.java
@@ -1,19 +1,18 @@
 package com.ocean_roast;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class RoasteryController {
-    @Autowired
-    RoasteryService roasteryService;
 
     @GetMapping("/roasteries")
-    public List<Roastery> getRoasteries() throws IOException {
-        return roasteryService.getRoasteries();
+    private List<Roastery> getRoasteries() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(new ClassPathResource("roasteries.json").getFile(), objectMapper.getTypeFactory().constructCollectionType(List.class, Roastery.class));
     }
 }
