5ead0145539ea48812e5691a1a28f406fe886713
Enis Arik <enisarik1987@gmail.com>
2025-01-09 22:14:47 +0300

    added all files

---
+++ src/main/java/com/ocean_roast/OceanRoastApplication.java
@@ -0,0 +1,13 @@
+package com.ocean_roast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanRoastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanRoastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocean_roast/OceanRoastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean_roast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanRoastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
