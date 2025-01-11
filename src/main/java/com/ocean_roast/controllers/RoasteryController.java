85cf864f3508d90b7cc8a06e38a8609047b96760
Enis Arik <enisarik1987@gmail.com>
2025-01-11 18:13:13 +0300

    fix issue with sl4j and cleanup pom.xml

--- src/main/java/com/ocean_roast/controllers/RoasteryController.java
+++ src/main/java/com/ocean_roast/controllers/RoasteryController.java
@@ -3,16 +3,20 @@ package com.ocean_roast.controllers;
 import com.ocean_roast.models.Roastery;
 import com.ocean_roast.models.RoasteryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RoasteryController {
 
     @GetMapping("/roasteries")
+
     public List<Roastery> getRoasteries() {
+        log.info("getRoasteries() called");
         return RoasteryFactory.getRoasteries();
     }
 }
